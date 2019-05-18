865f170db45126e213e4cff38e93953349448062
khaledbakeer <bakeer@khaled.de>
2019-05-18 20:04:00 +0200

    update

--- src/include/Calculate.java
+++ src/include/Calculate.java
@@ -1,5 +1,8 @@
 package include;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculate {
 
     private int _t = 0;
@@ -8,6 +11,15 @@ public class Calculate {
     private double[][] _transitionProbability;
     private double[] _stateProbability;
 
+    public List<double[]> resultsTable;
+
+    public Calculate(double[][] _transitionProbability, double[] StartStateProbability) {
+        this._j = _transitionProbability.length;
+        this._i = _transitionProbability[0].length;
+        this._transitionProbability = _transitionProbability;
+        this._stateProbability = StartStateProbability;
+        resultsTable = new ArrayList<>();
+    }
 
     public void PrintTransitionProbability() {
         System.out.println("Transition Probability: ");
@@ -21,6 +33,7 @@ public class Calculate {
     }
 
     public void PrintStateProbability() {
+        double[] tmp = new double[_i];
         for (int a = 0; a < get_transitionProbability().length; a++) {
             double x = 0;
             for (int b = 0; b < get_transitionProbability()[a].length; b++) {
@@ -29,24 +42,18 @@ public class Calculate {
                 else System.out.print(" + ");
                 x = x + (get_stateProbability()[b] * get_transitionProbability()[b][a]);
             }
+            tmp[a] = x;
             System.out.println(x);
             System.out.println();
         }
+        resultsTable.add(tmp);
     }
 
     public double[][] get_transitionProbability() {
         return _transitionProbability;
     }
 
-    public void set_transitionProbability(double[][] _transitionProbability) {
-        this._transitionProbability = _transitionProbability;
-    }
-
     public double[] get_stateProbability() {
         return _stateProbability;
-    }
-
-    public void set_StartStateProbability(double[] _StartStateProbability) {
-        this._stateProbability = _StartStateProbability;
     }
 }
