9b8ab36f9ff83531754d040ec97608987e47d223
khaledbakeer <bakeer@khaled.de>
2019-05-19 13:01:39 +0200

    add another example

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -4,12 +4,15 @@ public class AppRunner {
 
     public static void main(String[] args) {
 
-        double[] stateProbability = {0.5, 0.5};
+        double[] stateProbability = {0.3, 0.2, 0.25, 0.2, 0.05};
 
         double[][] transitionProbability =
                 {
-                        {0.5, 0.5},
-                        {0.5, 0.5}
+                        {0.0, 0.2, 0.5, 0.1, 0.2},
+                        {0.1, 0.4, 0.0, 0.3, 0.2},
+                        {0.2, 0.2, 0.2, 0.2, 0.2},
+                        {0.2, 0.3, 0.1, 0.3, 0.1},
+                        {0.2, 0.3, 0.1, 0.1, 0.3}
                 };
 
         Markow c = new Markow(transitionProbability, stateProbability);
