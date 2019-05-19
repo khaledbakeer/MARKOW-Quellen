44381cef8eec522620ade34eaddb803f0428e6a6
khaledbakeer <bakeer@khaled.de>
2019-05-19 10:35:08 +0200

    special case: `p1 = p2 = 0.5`, `p(x2,x1) = p(x1,x2) = p` and `p(x1,x1) = p(x2,x2) = 1 - p`

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -4,14 +4,12 @@ public class AppRunner {
 
     public static void main(String[] args) {
 
-        double[] stateProbability = {0.25, 0.25, 0.25, 0.25};
+        double[] stateProbability = {0.5, 0.5};
 
         double[][] transitionProbability =
                 {
-                        {0.0, 0.2, 0.7, 0.1},
-                        {0.1, 0.6, 0.0, 0.3},
-                        {0.2, 0.4, 0.2, 0.2},
-                        {0.2, 0.3, 0.1, 0.4}
+                        {0.5, 0.5},
+                        {0.5, 0.5}
                 };
 
         Markow c = new Markow(transitionProbability, stateProbability);
